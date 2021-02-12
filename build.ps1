79ce10aa5480f9a7db2e271aef24bcfca6ed11b9
Dirk <bremendirk@gmail.com>
2021-02-12 20:26:45 +0000

    Adding Jupyter PowerShell notebook blogging

--- build.ps1
+++ build.ps1
@@ -14,27 +14,35 @@ function build {
             $NotebookName,
             [Parameter(Mandatory)]
             $UrlPostFix,
-            $urlPrefix = 'https://powershellone.files.wordpress.com/'
-
-          
+            $UrlPrefix = 'https://powershellone.files.wordpress.com/'
         )
-    $imageUrlPrefix = $urlPrefix + $UrlPostFix
+    $imageUrlPrefix = $UrlPrefix + $UrlPostFix
     if (! $imageUrlPrefix.EndsWith('/')) {  $imageUrlPrefix =  $imageUrlPrefix+ '/'}
     $path = $PSScriptRoot
     Push-Location $path
     $NotebookName = $NotebookName.Replace('.\','')
+    $nbPath = (Resolve-Path $NotebookName)
+    $text = Get-Content $nbPath -Raw
+    $text = $text.Replace('$','~~')
+    $text | Set-Content $nbPath -NoNewline -Encoding utf8
     python .\run_nb2wp.py $NotebookName $imageUrlPrefix
+    $text = $text.Replace('~~' ,'$')
+    $text | Set-Content $nbPath -NoNewline  -Encoding utf8
     Add-Type -Path "C:\scripts\ps1\get-stats\Helper\HtmlAgilityPack.dll" 
     $doc = New-Object HtmlAgilityPack.HtmlDocument
     $htmlName = [System.IO.Path]::ChangeExtension($NotebookName, 'html')
     $htmlFolder = 'html\' + [System.IO.Path]::GetFileNameWithoutExtension($NotebookName)
     $htmlPath = Join-Path (Join-Path $path $htmlFolder) $htmlName
+    $text = Get-Content $htmlPath -Raw -Encoding utf8
+    $text = $text.Replace('~~' , '$')
+    $text | Set-Content $htmlPath -NoNewline  -Encoding utf8
     $result = $doc.Load($htmlPath)
     $result = $doc.DocumentNode.SelectNodes("//div[contains(@class,'prompt')]")
     $result.foreach{$_.remove()}
+    $doc.DocumentNode.SelectSingleNode('//body').OuterHtml | Set-Clipboard
     $doc.Save($htmlPath)
     move (Resolve-Path $NotebookName) notebooks
     Pop-Location
 }
 
-#build blog_test.ipynb 2021/02
+build 'Jupyter_PowerShell_Notebooks_based_blogging.ipynb' 2021/02
